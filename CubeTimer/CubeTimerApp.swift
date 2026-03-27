8ebdf7f2803251e0e8831d295e5859faa159fef2
dai <Dai.Ting@externals.adidas.com>
2026-03-27 15:20:21 +0800

    Initial Commit

---
+++ CubeTimer/CubeTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CubeTimerApp.swift
+//  CubeTimer
+//
+//  Created by jamadai on 2026/3/27.
+//
+
+import SwiftUI
+
+@main
+struct CubeTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
